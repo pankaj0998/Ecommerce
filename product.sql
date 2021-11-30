98f332ace3826fe3a074ecd921b79daddc58d2bb
Pankaj Kumar Chaurasia <54090060+pankaj0998@users.noreply.github.com>
2021-11-30 11:05:49 +0530

    MySql Scripts

---
+++ product.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM project.products
+LIMIT 0, 1000
+
+-- Date: 2021-11-23 17:16
+*/
+INSERT INTO `products` (`id`,`category`,`discription`,`price`,`product_name`) VALUES (1,'Electronics','4GB Ram, 64GB Rom',15000.00,'Oppo f9');
+INSERT INTO `products` (`id`,`category`,`discription`,`price`,`product_name`) VALUES (2,'Electronics','8GB Ram, 128GB Rom',21000.00,'Oppo f9 Pro');
+INSERT INTO `products` (`id`,`category`,`discription`,`price`,`product_name`) VALUES (3,'Furniture','Led Lamp',210.00,'Table Lamp');
+INSERT INTO `products` (`id`,`category`,`discription`,`price`,`product_name`) VALUES (4,'Furniture','Amazon Brand - Solimo Loft Chair',3719.00,'Office Chair');
+INSERT INTO `products` (`id`,`category`,`discription`,`price`,`product_name`) VALUES (5,'Clothing','Mens Polo Tshirt - Collar t-Shirt Regular fit',350.00,'Polo ');
---
+++ user.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM project.users
+LIMIT 0, 1000
+
+-- Date: 2021-11-23 17:18
+*/
+INSERT INTO `users` (`id`,`country`,`email`,`firstname`,`lastname`,`phonenumber`,`state`,`street`,`username`) VALUES (101,'India','rajan123@gmail.com','Rajan','Shetty','9876543210','Karnataka','Banglore','rajan123');
+INSERT INTO `users` (`id`,`country`,`email`,`firstname`,`lastname`,`phonenumber`,`state`,`street`,`username`) VALUES (102,'India','Ajay98@gmail.com','Ajay','Gupta','9087654321','Maharashtra','Mumbai','ajay@98');
+INSERT INTO `users` (`id`,`country`,`email`,`firstname`,`lastname`,`phonenumber`,`state`,`street`,`username`) VALUES (103,'India','rahulchopra96@yahoo.com','Rahul','Chopra','8726161729','Maharshtra','Thane','rahulch96@');
